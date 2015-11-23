3270707d028e948204db81104e38f6d4b6ca1aaa
tsuji <tsuji.daisuke75@gmail.com>
2015-11-23 14:53:35 +0900

    initial commit 新規登録画面まで完成。

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,40 @@
+class CustomersController < ApplicationController
+  def index
+  end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      redirect_to @customer
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def destroy
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(
+      :family_name,
+      :given_name,
+      :email
+      )
+  end
+
+end
